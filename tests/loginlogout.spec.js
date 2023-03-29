cd54bcd14625a44ee62e2964c475589485248b4f
Philip Lushevsky <philip.lushevsky@nixsolutions.com>
2023-03-29 13:17:55 +0200

    19

--- tests/loginlogout.spec.js
+++ tests/loginlogout.spec.js
@@ -1,30 +1,19 @@
-import {
-  test,
-  expect
-} from '@playwright/test';
+import { test, expect } from '@playwright/test';
 import LoginPage from '../pages/loginPage';
 import LibraryPage from '../pages/libraryPage';
-import {
-  ENV
-} from '../envconfigs/envs';
+import { ENV } from '../envconfigs/envs';
 
-test('loginlogout @smoke', async ({
-  page
-}) => {
+test('loginlogout @smoke', async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await page.goto('/');
   await expect(loginPage.heading).toBeVisible();
 
   await page.locator('#onetrust-accept-btn-handler').click();
-  await page.getByRole('button', {
-    name: 'Log In'
-  }).first().click();
-  await page.getByPlaceholder('yours@example.com').fill(ENV.USERNAME);
-  await page.getByPlaceholder('your password').fill(ENV.PASSWORD);
-  await page.getByRole('button', {
-    name: 'Log In'
-  }).click();
+  await page.getByRole('button', {name: 'Log In'}).first().click();
+  await page.getByPlaceholder('User ID / Email').fill(ENV.USERNAME);
+  await page.getByPlaceholder('Password').fill(ENV.PASSWORD);
+  await page.getByRole('button', {name: 'Log In'}).click();
 
   //await page.waitForSelector('h1');
 
@@ -33,5 +22,5 @@ test('loginlogout @smoke', async ({
   //await page.getByTestId('logout-btn').click();
 
   // ---------------------
-  await expect(loginPage.heading).toBeVisible();
+  //await expect(loginPage.heading).toBeVisible();
 });

02437781f823481fd5fc99fff45fc55acdc9e948
Philip Lushevsky <philip.lushevsky@nixsolutions.com>
2023-04-05 23:33:51 +0200

    clear

--- constants/url.js
+++
@@ -1,4 +0,0 @@
-export const URL = {
-  ebooks: `https://${process.env.test_env}-ebooks.health.elsevier.com/`,
-  espAdmin: `https://${process.env.test_env}-esp-admin.health.elsevier.com/`
-};
--- uimodules/acceptCookiesBanner.js
+++
@@ -1,7 +0,0 @@
-export default class AcceptCookiesBanner {
-  constructor(page) {
-    this.page = page;
-    this.acceptAllButton = page.locator('#onetrust-accept-btn-handler');
-    this.cookiesSettingsButton = page.locator('#onetrust-pc-btn-handler');
-  }
-}
